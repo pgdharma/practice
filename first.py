bd01c316011b96c749cdcc56750b991fc50753a1
pgdharma <112254452+pgdharma@users.noreply.github.com>
2025-01-10 11:59:40 +0530

    simple calculator

--- first.py
+++ first.py
@@ -2,7 +2,6 @@
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number : "))
 operation = input("Enter operation (+,-,*,/): ")
-
 if operation == '+':
     result = num1 + num2
 elif operation == '-':
@@ -13,5 +12,4 @@ elif operation == '/':
     result = num1 / num2 if num2 != 0 else "Cannot divide by zero"
 else:
     result = "Invalid operation"
-
 print("Result:", result)
